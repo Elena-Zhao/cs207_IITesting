41c2dfd269d0062ed04cad2d10bf14701a421646
Elena-Zhao <zhaoqing513@126.com>
2016-02-28 21:34:01 -0500

    add more test cases

--- test_binsearch.py
+++ test_binsearch.py
@@ -23,17 +23,17 @@ def test_inf():
 def test_inf_2():
     assert binary_search([1,2,np.inf], np.inf) == 2
 
-def test_boundary_1:
+def test_boundary_1():
     assert binary_search(input, 5, 1, 3) == -1
 
-def test_boundary_2:
+def test_boundary_2():
     assert binary_search(input, 2, 1, 3) == 2
 
-def test_reversed_index:
+def test_reversed_index():
     assert binary_search(input, 2, 3, 1) == -1
 
-def test_same_index:
+def test_same_index():
     assert binary_search(input, 2, 2, 2) == 2
     
-def test_same_index_2:
+def test_same_index_2():
     assert binary_search(input, 5, 2, 2) == -1
