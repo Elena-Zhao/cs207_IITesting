a429232c2218f5fbe67803e33b282178965a31a4
Elena-Zhao <zhaoqing513@126.com>
2016-02-28 22:05:01 -0500

    add more test cases

--- test_binsearch.py
+++ test_binsearch.py
@@ -40,3 +40,44 @@ def test_same_index():
     
 def test_same_index_2():
     assert binary_search(input, 5, 2, 2) == -1
+
+def test_None():
+    with raises(TypeError):
+        binary_search([2,3,1, None], None)
+        
+def test_None_2():
+    with raises(TypeError):
+        binary_search([2,3, None, 5], 5)
+
+def test_None_3():
+    assert binary_search([2,3,5, None], 2) ==0
+    
+def test_unsorted():
+    assert binary_search([2,3,5, 0, 10], 3) ==1
+
+def test_unsorted_2():
+    assert binary_search([2,3,5, 0, 10], 10) == 4
+    
+def test_unsorted_3():
+    assert binary_search([2,3,5, 0, -1], -1) == -1
+    
+def test_string():
+    with raises(TypeError):
+        binary_search([2,3, 'a', 5], 'a')
+        
+def test_string_2():
+     with raises(TypeError):
+        binary_search([2,3, 'a', 5], 5)
+        
+def test_string_3():
+    assert binary_search([2,3, 'a', 5], 2) == 0
+
+def test_duplicate():
+    assert binary_search([-1,2,3, 5, 5,5, 6], 5) == 3
+    
+def test_overflow():
+    with raises(OverflowError):
+        binary_search(range(100000000000000000000000), 5)
+
+def test_float():
+    assert binary_search([1,2,3,5.0], 5)== 3
